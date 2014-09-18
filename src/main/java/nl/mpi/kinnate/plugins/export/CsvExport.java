6d77517872308803a2ab362f2f13e7e986d38a2a
Peter Withers <Peter.Withers@mpi.nl>
2014-09-18 07:16:00 +0100

    Added ui components to the CSV export plugin.

--- src/main/java/nl/mpi/kinnate/plugins/export/CsvExport.java
+++ src/main/java/nl/mpi/kinnate/plugins/export/CsvExport.java
@@ -29,19 +29,31 @@ import org.basex.core.BaseXException;
  */
 public class CsvExport extends AbstractBaseModule implements KinOathPanelPlugin {
 
+    private String exportProject;
+
     public CsvExport() throws PluginException {
         super("CSV file export", "Exports CSV files.", "nl.mpi.kinnate.plugins.export");
     }
 
-    public JScrollPane getUiPanel(PluginDialogHandler dialogHandler, PluginSessionStorage sessionStorage, PluginBugCatcher bugCatcher) throws PluginException {
+    public JScrollPane getUiPanel(PluginDialogHandler dialogHandler, final PluginSessionStorage sessionStorage, PluginBugCatcher bugCatcher) throws PluginException {
         final CollectionExport entityCollection = new CollectionExport(bugCatcher, sessionStorage);
         final FileSelectPanel fileSelectPanel = new FileSelectPanel(dialogHandler, sessionStorage, "CsvExport");
+        exportProject = sessionStorage.loadString("nl.mpi.kinnate.plugins.export.CsvExport.exportProject");
         final JPanel pluginPanel = new JPanel(new BorderLayout());
-        final JTextArea textPane = new JTextArea("This plugin will export the entire project into a CSV file for processing in R.\n"
-                + "To use, slect the project then browse for the file into which you wish to export your data and click \"run\".");
+        final JTextArea textPane = new JTextArea("This plugin will export an entire project into a CSV file for processing in R.\n"
+                + "To use, select the project then browse for the file into which you wish to export your data and click \"run\".");
         try {
             final String[] databaseList = entityCollection.listDatabases();
-            pluginPanel.add(new JComboBox(databaseList), BorderLayout.LINE_START);
+            final JComboBox projectSelectBox = new JComboBox(databaseList);
+            projectSelectBox.setSelectedItem(exportProject);
+            projectSelectBox.setAction(new AbstractAction() {
+
+                public void actionPerformed(ActionEvent e) {
+                    exportProject = projectSelectBox.getSelectedItem().toString();
+                    sessionStorage.saveString("nl.mpi.kinnate.plugins.export.CsvExport.exportProject", exportProject);
+                }
+            });
+            pluginPanel.add(projectSelectBox, BorderLayout.LINE_START);
         } catch (BaseXException baseXException) {
             textPane.setText(baseXException.getMessage());
         }
