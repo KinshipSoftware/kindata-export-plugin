3d787f2252fb9246b187266aa0a97272733e5287
Peter Withers <Peter.Withers@mpi.nl>
2014-09-18 06:37:06 +0100

    Added an xquery based export plugin.

--- src/main/java/nl/mpi/kinnate/plugins/export/QueryExport.java
+++ src/main/java/nl/mpi/kinnate/plugins/export/QueryExport.java
@@ -1,15 +1,15 @@
 package nl.mpi.kinnate.plugins.export;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextPane;
 import nl.mpi.flap.kinnate.entityindexer.QueryException;
 import nl.mpi.flap.module.AbstractBaseModule;
 import nl.mpi.flap.plugin.KinOathPanelPlugin;
@@ -18,7 +18,7 @@ import nl.mpi.flap.plugin.PluginDialogHandler;
 import nl.mpi.flap.plugin.PluginException;
 import nl.mpi.flap.plugin.PluginSessionStorage;
 import nl.mpi.kinnate.entityindexer.CollectionExport;
-import org.basex.core.BaseXException;
+import nl.mpi.kinnate.plugins.export.ui.FileSelectPanel;
 
 /**
  * Document : QueryExport Created on : Sept 17, 2014, 21:53 PM
@@ -33,22 +33,32 @@ public class QueryExport extends AbstractBaseModule implements KinOathPanelPlugi
 
     public JScrollPane getUiPanel(PluginDialogHandler dialogHandler, PluginSessionStorage sessionStorage, PluginBugCatcher bugCatcher) throws PluginException {
         final CollectionExport entityCollection = new CollectionExport(bugCatcher, sessionStorage);
-        final JPanel pluginPanel = new JPanel();
+        final FileSelectPanel fileSelectPanel = new FileSelectPanel(dialogHandler, sessionStorage, "QueryExport");
+        final JPanel pluginPanel = new JPanel(new BorderLayout());
         final JEditorPane jEditorPane = new JEditorPane("text/plain", "db:list()");
-        pluginPanel.add(jEditorPane);
+        pluginPanel.add(new JScrollPane(jEditorPane), BorderLayout.CENTER);
         final JLabel errorLabel = new JLabel();
         pluginPanel.add(new JButton(new AbstractAction("run") {
 
             public void actionPerformed(ActionEvent e) {
-                errorLabel.setText("");
+                errorLabel.setText("running...");
                 try {
-                    System.out.println(entityCollection.performExportQuery(jEditorPane.getText()));
+                    System.out.println("fileSelectPanel:" + fileSelectPanel.getExportFile().toString());
+                    final String exportQueryResult = entityCollection.performExportQuery(jEditorPane.getText());
+                    System.out.println(exportQueryResult);
+                    FileWriter fileWriter = new FileWriter(fileSelectPanel.getExportFile());
+                    fileWriter.write(exportQueryResult);
+                    fileWriter.close();
+                    errorLabel.setText("Export file complete.\n");
                 } catch (QueryException exception) {
+                    errorLabel.setText(exception.getMessage());
+                } catch (IOException exception) {
                     errorLabel.setText(exception.getMessage());
                 }
             }
-        }));
-        pluginPanel.add(errorLabel);
+        }), BorderLayout.LINE_END);
+        pluginPanel.add(fileSelectPanel, BorderLayout.PAGE_START);
+        pluginPanel.add(errorLabel, BorderLayout.PAGE_END);
         return new JScrollPane(pluginPanel);
     }
 }
